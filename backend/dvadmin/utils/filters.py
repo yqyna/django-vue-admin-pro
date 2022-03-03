f1e84e1fe336f95dc9b4260000f7fcddd17f4af3
李强 <1206709430@qq.com>
2022-03-03 17:13:23 +0800

    功能变化: 优化CustomDjangoFilterBackend，添加不进行过滤的model 类

--- backend/dvadmin/utils/filters.py
+++ backend/dvadmin/utils/filters.py
@@ -7,6 +7,7 @@
 @Remark: 自定义过滤器
 """
 import operator
+from collections import OrderedDict
 from functools import reduce
 
 import six
@@ -15,6 +16,7 @@ from django.db.models.constants import LOOKUP_SEP
 from django_filters import utils
 from django_filters.filters import CharFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.utils import get_model_field
 from rest_framework.filters import BaseFilterBackend
 
 from dvadmin.system.models import Dept
@@ -122,6 +124,104 @@ class CustomDjangoFilterBackend(DjangoFilterBackend):
                 return lookup
         return None
 
+    def get_filterset_class(self, view, queryset=None):
+        """
+        Return the `FilterSet` class used to filter the queryset.
+        """
+        filterset_class = getattr(view, 'filterset_class', None)
+        filterset_fields = getattr(view, 'filterset_fields', None)
+
+        # TODO: remove assertion in 2.1
+        if filterset_class is None and hasattr(view, 'filter_class'):
+            utils.deprecate(
+                "`%s.filter_class` attribute should be renamed `filterset_class`."
+                % view.__class__.__name__)
+            filterset_class = getattr(view, 'filter_class', None)
+
+        # TODO: remove assertion in 2.1
+        if filterset_fields is None and hasattr(view, 'filter_fields'):
+            utils.deprecate(
+                "`%s.filter_fields` attribute should be renamed `filterset_fields`."
+                % view.__class__.__name__)
+            filterset_fields = getattr(view, 'filter_fields', None)
+
+        if filterset_class:
+            filterset_model = filterset_class._meta.model
+
+            # FilterSets do not need to specify a Meta class
+            if filterset_model and queryset is not None:
+                assert issubclass(queryset.model, filterset_model), \
+                    'FilterSet model %s does not match queryset model %s' % \
+                    (filterset_model, queryset.model)
+
+            return filterset_class
+
+        if filterset_fields and queryset is not None:
+            MetaBase = getattr(self.filterset_base, 'Meta', object)
+
+            class AutoFilterSet(self.filterset_base):
+
+                @classmethod
+                def get_filters(cls):
+                    """
+                    Get all filters for the filterset. This is the combination of declared and
+                    generated filters.
+                    """
+
+                    # No model specified - skip filter generation
+                    if not cls._meta.model:
+                        return cls.declared_filters.copy()
+
+                    # Determine the filters that should be included on the filterset.
+                    filters = OrderedDict()
+                    fields = cls.get_fields()
+                    undefined = []
+
+                    for field_name, lookups in fields.items():
+                        field = get_model_field(cls._meta.model, field_name)
+                        from django.db import models
+                        from timezone_field import TimeZoneField
+                        # 不进行 过滤的model 类
+                        if isinstance(field, (models.JSONField, TimeZoneField)):
+                            continue
+                        # warn if the field doesn't exist.
+                        if field is None:
+                            undefined.append(field_name)
+
+                        for lookup_expr in lookups:
+                            filter_name = cls.get_filter_name(field_name, lookup_expr)
+
+                            # If the filter is explicitly declared on the class, skip generation
+                            if filter_name in cls.declared_filters:
+                                filters[filter_name] = cls.declared_filters[filter_name]
+                                continue
+
+                            if field is not None:
+                                filters[filter_name] = cls.filter_for_field(field, field_name, lookup_expr)
+
+                    # Allow Meta.fields to contain declared filters *only* when a list/tuple
+                    if isinstance(cls._meta.fields, (list, tuple)):
+                        undefined = [f for f in undefined if f not in cls.declared_filters]
+
+                    if undefined:
+                        raise TypeError(
+                            "'Meta.fields' must not contain non-model field names: %s"
+                            % ', '.join(undefined)
+                        )
+
+                    # Add in declared filters. This is necessary since we don't enforce adding
+                    # declared filters to the 'Meta.fields' option
+                    filters.update(cls.declared_filters)
+                    return filters
+
+                class Meta(MetaBase):
+                    model = queryset.model
+                    fields = filterset_fields
+
+            return AutoFilterSet
+
+        return None
+
     def filter_queryset(self, request, queryset, view):
         filterset = self.get_filterset(request, queryset, view)
         if filterset is None:
@@ -130,7 +230,7 @@ class CustomDjangoFilterBackend(DjangoFilterBackend):
             queryset = filterset.queryset
             orm_lookups = []
             for search_field in filterset.filters:
-                if isinstance(filterset.filters[search_field],CharFilter):
+                if isinstance(filterset.filters[search_field], CharFilter):
                     orm_lookups.append(self.construct_search(six.text_type(search_field)))
                 else:
                     orm_lookups.append(search_field)
